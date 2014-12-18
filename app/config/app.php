543b65c2da515c0b486a055e60a160c985650a48
Eduardo Cruz <eduardo@eduardocruz.com>
2014-12-18 14:08:03 -0300

    Adding service provider

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-
+		'Eduardocruz\Laravel4bootstrap3\laravel4bootstrap3ServiceProvider'
 	),
 
 	/*
